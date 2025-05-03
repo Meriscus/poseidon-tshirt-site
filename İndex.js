5b5afaa9cc6b378a038ad2892e7ebde22f682c38
Meriscus <mtnfix2@gmail.com>
2025-05-03 13:18:37 +0300

    Create İndex.js

---
+++ İndex.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center", flexDirection: "column", background: "#111", color: "#fff" }}>
+      <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>Poseidon Koleksiyonu</h1>
+      <p style={{ fontSize: "1.2rem" }}>Yunan mitolojisinden ilham alan tişört tasarımı</p>
+    </main>
+  );
+}
